0ac6749bea84820c7b91091f15c13987e2794c80
CJ-ormandah <jessben.cn@gmail.com>
2022-12-16 16:11:59 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns all alphabets except e and q
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10 ; a++)
+		putchar('0' + a);
+	putchar('\n');
+	return (0);
+}
